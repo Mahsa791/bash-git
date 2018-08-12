b6b260ad05a820f85c70fb4566bd470554ef52c2
Mahsa791 <mahsa.alborz@gmail.com>
2018-08-12 03:24:32 -0400

    First commit

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@
 git init
 git add .
 git commit -m "First commit"
-git remote add origin https://github.com/Mahsa791/Test-ma.git
+git remote add origin https://github.com/Mahsa791/bash-git.git
 git push origin HEAD:master
 
 
